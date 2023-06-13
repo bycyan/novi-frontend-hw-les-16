282e6b5b4c8d9cebd9f35733840524dc724cadf4
Cyan Dalebout <cyan@wervelwindmedia.nl>
2023-06-13 11:54:21 +0200

    homework finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { AuthContext } from './context/AuthContext';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </div>
-    </>
-  );
+    const { isAuth } = useContext(AuthContext);
+
+    return (
+        <>
+            {/*<NavBar />*/}
+            {/*<div className="content">*/}
+            {/*    <Routes>*/}
+            {/*        <Route exact path="/">*/}
+            {/*            /!*<Home />*!/*/}
+            {/*        </Route>*/}
+            {/*        <Route path="/profile">*/}
+            {/*            {isAuth ? <Profile /> : <Navigate to="/" />}*/}
+            {/*        </Route>*/}
+            {/*        <Route exact path="/signin">*/}
+            {/*            <SignIn />*/}
+            {/*        </Route>*/}
+            {/*        <Route exact path="/signup">*/}
+            {/*            <SignUp />*/}
+            {/*        </Route>*/}
+            {/*    </Routes>*/}
+            {/*</div>*/}
+
+            <div>
+                <NavBar />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route
+                            path="/profile"
+                            element={isAuth ? <Profile /> : <Navigate to="/" />}
+                        />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/signup" element={<SignUp />} />
+                    </Routes>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '../assets/banana-01.png';
 import { useNavigate, Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 function NavBar() {
+  const { isAuth, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
-    <nav>
+      <nav>
         <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
@@ -16,21 +18,30 @@ function NavBar() {
           </span>
         </Link>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => navigate('/signin')}
-        >
-          Log in
-        </button>
-        <button
-          type="button"
-          onClick={() => navigate('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
-    </nav>
+        {isAuth ?
+            <button
+                type="button"
+                onClick={logout}
+            >
+              Log uit
+            </button>
+            :
+            <div>
+              <button
+                  type="button"
+                  onClick={() => navigate('/signin')}
+              >
+                Log in
+              </button>
+              <button
+                  type="button"
+                  onClick={() => navigate('/signup')}
+              >
+                Registreren
+              </button>
+            </div>
+        }
+      </nav>
   );
 }
 
